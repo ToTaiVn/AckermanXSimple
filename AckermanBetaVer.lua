7f96c89f427321a56da9fae008f240c931767277
Ahilla <139516438+ToTaiVn@users.noreply.github.com>
2023-12-23 19:21:28 +0700

    Update AckermanBetaVer.lua

--- AckermanBetaVer.lua
+++ AckermanBetaVer.lua
@@ -1,3 +1,15 @@
+
+wait(1.2)
+if getgenv().OldScript == true then
+  
+_G.Team = "Pirates" --Marines
+_G.Language = "English" --Vietnamese
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ToTaiVn/Sasageyo/main/Main'))()
+else
+  if getgenv().OldScript == false then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ToTaiVn/AckermanXSimple/main/Protected_2164516921352235.lua.txt"))()
+  end
+end
+
 print("Ackerman Hub On Top!!")
-wait(.1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ToTaiVn/AckermanXSimple/main/Kono-Baka.lua"))()
