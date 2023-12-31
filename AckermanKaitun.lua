fe6b6e0e8ed455f6c9cbaf12b51446684cf37d63
Ahilla <139516438+ToTaiVn@users.noreply.github.com>
2023-12-31 20:43:09 +0700

    Update AckermanKaitun.lua

--- AckermanKaitun.lua
+++ AckermanKaitun.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ToTaiVn/AckermanXSimple/main/AckermanKaitunV2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToTaiVn/AckermanXSimple/main/XinLoiViQuaDepTrai.lua"))()
