40fc81470334a2a52e42b7e4b9a895aff87e3617
Ahilla <139516438+ToTaiVn@users.noreply.github.com>
2024-01-01 08:02:13 +0700

    Update AckermanKaitun.lua

--- AckermanKaitun.lua
+++ AckermanKaitun.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ToTaiVn/AckermanXSimple/main/kaitunnhucucac.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToTaiVn/AckermanXSimple/main/kaitunnhuconcac.lua"))()
